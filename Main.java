7dbb844eba9ff9e515bed8b495b35d326b4d0458
Levi-Mid <abdomlevi1999@gmail.com>
2026-08-13 00:50:14 -0300

    Finalizado até a multiplização
    Criado 6 funções recursivas em java com cálculos básicos iniciais

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main (String[] args){
+
+        System.out.println(MatematicaReconstruida.multiplicacao(3,5));
+        System.out.println(MatematicaReconstruida.multiplicacao(3,-5));
+        System.out.println(MatematicaReconstruida.multiplicacao(-3,5));
+        System.out.println(MatematicaReconstruida.multiplicacao(-3,-5));
         
     }
 }
--- MatematicaReconstruida.java
+++ MatematicaReconstruida.java
@@ -11,4 +11,43 @@ public class MatematicaReconstruida {
         d--;
         return MatematicaReconstruida.isNegativo(a, d);
     }
+    public static int soma(int a, int b){
+        if (a==0) return b;
+        if (b==0) return a;
+
+        return MatematicaReconstruida.soma(++a, --b);
+    }
+    public static int subtracao(int a, int b){
+        if (a==0) return b;
+        if (b==0) return a;
+        if (isNegativo(a)){
+            if (isNegativo(b)) {
+                return subtracao(--a, ++b); 
+           }return subtracao(++a, --b);
+        }
+        if (isNegativo(b)) return subtracao(--a, ++b);
+        return subtracao(--a, --b);
+    }
+    public static int modulo(int x){
+        if (x==0) return 0;
+        if (isNegativo(x)){
+            return soma(1, modulo(++x));
+        }
+        return x;
+    }
+    public static int multiplicacao(int a, int b){
+  
+        if (a==0) return 0;
+        if (b==0) return 0;
+        if ( isNegativo(a)){
+            if (isNegativo(b)) {
+                return multiplicacao(modulo(a), modulo(b));
+            }
+            return subtracao(multiplicacao(a, --b), a);
+        }
+        if (isNegativo(b)){
+            return multiplicacao(b, a);
+        }
+        return soma(multiplicacao(a, --b), a);
+    }
 }
